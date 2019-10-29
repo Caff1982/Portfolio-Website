4820ca4e86b7c4bbfc8cc638c2faef8feae59cc9
Caff1982 <stephencafferty@hotmail.com>
2019-10-29 08:58:34 +0000

    Added simple modal popup

--- dist/js/main.js
+++ dist/js/main.js
@@ -32,3 +32,35 @@ function toggleMenu() {
   showMenu = false;
   }
 }
+
+
+// Get modal elements
+var modal  = document.getElementById('simpleModal');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// listen for open click
+modalBtn.addEventListener('click', openModal);
+// listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// function to close modal if outside click
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
